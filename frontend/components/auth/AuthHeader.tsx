5824ff2ab090b110549af2185e85ebedb76696dc
{Jayendra <jay.bharti2804@gmail.com>
2025-09-22 10:15:16 +0530

    header logo changed

--- frontend/components/auth/AuthHeader.tsx
+++ frontend/components/auth/AuthHeader.tsx
@@ -1,19 +1,11 @@
-import Link from "next/link";
 import ThemeSwitch from "../ThemeSwitch";
-import { cn } from "@/lib/utils";
-import { anurati } from "@/utils/fonts";
-import { appName } from "@/utils/data";
+import Logo from "../Logo";
 
 export default function AuthHeader() {
   return (
     <header className="bg-background border-border border-b shadow-md">
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <Link
-          href={"/"}
-          className={cn("text-foreground text-xl font-bold", anurati.className)}
-        >
-          {appName}
-        </Link>
+        <Logo />
 
         <ThemeSwitch />
       </div>

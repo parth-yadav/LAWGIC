95f91ffc1c2f48fb73b22755eea3a7349db635a4
{Jayendra <jay.bharti2804@gmail.com>
2025-09-22 18:30:05 +0530

    fixed next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: `byte-us-dev-bucket.s3.ap-south-1.amazonaws.com`,
       },
+      {
+        protocol: "https",
+        hostname: `lawgic-backend-684404792129.asia-south2.run.app`,
+      },
     ],
   },
 };
